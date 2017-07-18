a1ed2fdd3a8fca1554992e57a3848221a4e41924
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2017-07-18 10:56:16 +0900

    modify comments a little

--- 2017/G/Main.java
+++ 2017/G/Main.java
@@ -1,5 +1,9 @@
 import java.util.*;
 
+/**
+ * ACM-ICPC 2017 Tsukuba, Japan Online First-Round Contest
+ * Problem G: Go around the Labyrinth
+ */
 class Main {
   public static void main(String[] arg) {
     Scanner scanner = new Scanner(System.in);
@@ -49,7 +53,8 @@ class Explorer {
    * (最初に南東隅に行くと，北東と南西の両方に行くことは不可能。
    *  ある順路が存在すれば逆にたどることも可能なので，最後に南東隅から
    *  北西出口に行くことも不可能。
-   *  逆にたどることが可能なので，反時計回りの順路があるなら時計回りの順路もある。)
+   *  逆にたどることが可能なので，反時計回りの順路があるなら時計回りの
+   *  順路もある。)
    * (なるべく外周に近いところを通る方が，通過不可能になる面積が小さくなる。)
    */
   boolean solve() {
@@ -66,9 +71,8 @@ class Explorer {
     while (true) {
       // 1回目は 2 * color, 2回目 (引き返すとき) は 2 * color + 1 を塗る
       map[y][x] = 2 * color + (map[y][x] != 0 ? 1 : 0);
-      // 左手の法則に従って探索する。
       int d;
-      for (d = -1; d <= 2; d++) { // 左→前→右→後の順に試す
+      for (d = -1; d <= 2; d++) { // 左→前→右→後の順に試す (左手の法則)
         int dd = (dir + d + 4) % 4;  // dd = dir + d (mod 4)
         int xx = x + vx[dd];
         int yy = y + vy[dd];
@@ -78,7 +82,7 @@ class Explorer {
           if (color == 4) { return true; }
           color++; // 次の宝を目指す
         }
-        if (0 < map[yy][xx] && map[yy][xx] < 2 * color) { continue; } // 通過済み
+        if (0 < map[yy][xx] && map[yy][xx] < 2 * color) { continue; } // 通過済
         if (map[yy][xx] > 2 * color) { continue; }  // すでに2回通った
         x = xx;
         y = yy;
