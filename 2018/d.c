414340105083425b90034756b33c10c9e6932b75
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2018-07-06 23:53:53 +0900

    C program for Problem D

---
+++ 2018/d.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+
+#define MAX_N  9
+#define MAX_MATCH  (MAX_N * (MAX_N - 1) / 2)
+
+#define None  (-1)
+
+struct Match {
+  int x, y;
+} match[MAX_MATCH];
+
+int count;
+int table[MAX_N][MAX_N];
+
+struct Match emptyCell(int n)
+{
+  struct Match m;
+  int x, y;
+  for (y = 0; y < n; y++) {
+    for (x = 0; x < n; x++) {
+      if (x != y && table[x][y] == None) {
+        m.x = x;
+        m.y = y;
+        return m;
+      }
+    }
+  }
+  m.x = m.y = -1;
+  return m;
+}
+
+int valid(int n, int x)
+{
+  int n0, n1;
+  int y;
+  n0 = n1 = 0;
+  for (y = 0; y < n; y++) {
+    if (table[x][y] == 0) { n0++; }
+    if (table[x][y] == 1) { n1++; }
+  }
+  return n0 <= (n-1)/2 && n1 <= (n-1)/2;
+}
+
+void search(int n)
+{
+  struct Match m;
+  int x, y;
+  m = emptyCell(n);
+  x = m.x;
+  y = m.y;
+  if (x < 0) {
+    count += 1;
+    return;
+  }
+  table[x][y] = 1;
+  table[y][x] = 0;
+  if (valid(n, x) && valid(n, y)) {
+    search(n);
+  }
+  table[x][y] = 0;
+  table[y][x] = 1;
+  if (valid(n, x) && valid(n, y)) {
+    search(n);
+  }
+  table[x][y] = None;
+  table[y][x] = None;
+}
+
+int solve(int n, struct Match match[], int m)
+{
+  int i, j;
+  count = 0;
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      table[i][j] = None;
+    }
+  }
+  for (i = 0; i < m; i++) {
+    table[match[i].x-1][match[i].y-1] = 1;
+    table[match[i].y-1][match[i].x-1] = 0;
+  }
+  search(n);
+  return count;
+}
+
+int main()
+{
+  int i, n, m, x, y;
+  for (;;) {
+    scanf("%d", &n);
+    if (n == 0) { break; }
+    scanf("%d", &m);
+    for (i = 0; i < m; i++) {
+      scanf("%d%d", &x, &y);
+      match[i].x = x;
+      match[i].y = y;
+    }
+    printf("%d\n", solve(n, match, m));
+  }
+  return 0;
+}
