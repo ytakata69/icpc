22fdf5d1f537eaf69ea70fd7fd565788e332bb94
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2018-07-07 11:57:46 +0900

    a little refactoring

--- 2018/d.py
+++ 2018/d.py
@@ -1,47 +1,53 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# 2018 ACM-ICPC国内予選
+# Problem D: 全チームによるプレーオフ
 
 def solve(n, match):
-    global count, table, n01
-    count = 0
+    '''param n チーム数
+       param match 実施済試合結果
+       return 全プレーオフになる組合せ数'''
+    global table, nwon, nlost
     table = [[None for a in range(n)] for t in range(n)]
     for x, y in match:
-        table[x-1][y-1] = 1
-        table[y-1][x-1] = 0 
-    n01 = [[sum(1 for y in range(n) if table[x][y] == i) for x in range(n)]
-           for i in range(2)]
-    search(0)
-    return count
+        table[x-1][y-1] = 1  # won
+        table[y-1][x-1] = 0  # lost
+    nwon, nlost = [[sum(1 for y in range(n) if table[x][y] == i)
+                    for x in range(n)] for i in range(2)]
+    return search(pos=0)
 
 def valid(x):
-    global table, n01
+    '''チームxの勝数・負数が(n-1)//2以下か'''
+    global table, nwon, nlost
     n = len(table)
-    return n01[0][x] <= (n-1)//2 and n01[1][x] <= (n-1)//2
+    return nwon[x] <= (n-1)//2 and nlost[x] <= (n-1)//2
 
 def search(pos):
-    global count, table, n01
+    '''param pos table中の次に埋める位置
+       return 全プレーオフになる組合せ数'''
+    global table, nwon, nlost
     n = len(table)
     x = pos // n
     y = pos %  n
     if pos >= n * n:
-        count += 1
-        return
+        return 1
     if x == y or table[x][y] != None:
-        search(pos + 1)
-        return
+        return search(pos + 1)
 
+    count = 0  # 組合せ数
     for i in range(2):
         table[x][y] = 1 - i
         table[y][x] = i
-        n01[1-i][x] += 1
-        n01[  i][y] += 1
+        nwon [(x, y)[i]] += 1
+        nlost[(y, x)[i]] += 1
         if valid(x) and valid(y):
-            search(pos + 1)
-        n01[1-i][x] -= 1
-        n01[  i][y] -= 1
+            count += search(pos + 1)
+        nwon [(x, y)[i]] -= 1
+        nlost[(y, x)[i]] -= 1
         table[x][y] = None
         table[y][x] = None
+    return count
 
 while True:
     n = int(input())
