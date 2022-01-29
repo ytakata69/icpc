a6abcf360cad4f036be8caf5f0369a806fce1df8
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2022-01-29 09:25:56 +0900

    Simplify 2017/A/main.py

--- 2017/A/main.py
+++ 2017/A/main.py
@@ -1,31 +1,16 @@
-#!/usr/bin/python
-# coding: utf-8
+#!/usr/bin/env python3
 
 # ACM-ICPC 2017 Tsukuba, Japan Online First-Round Contest
 # A: Taro's shopping
 
-import sys
-
-# @param n  商品数
-# @param m  金額
-# @param a  各商品の価格
 def solve(n, m, a):
-  max = 0;
-  # すべての組(i,j)を調べる
-  for i in range(n):
-    for j in range(i + 1, n):
-      p = a[i] + a[j]
-      if p <= m and max < p:
-        max = p
-  print("%d" % max if max > 0 else "NONE")
-
-
-def splitIntoNums(line):
-  words = line.strip().split()            # split into words
-  return [int(w) for w in words]          # convert to integers
+    return max([a[i] + aj for  i in range(n)
+                          for aj in a[i + 1:] if a[i] + aj <= m],
+               default="NONE")
 
 while True:
-  n, m = splitIntoNums(sys.stdin.readline())
-  if n == 0 and m == 0: break
-  a = splitIntoNums(sys.stdin.readline())
-  solve(n, m, a)
+    n, m = map(int, input().split())
+    if n == 0 and m == 0:
+        break
+    a = list(map(int, input().split()))
+    print(solve(n, m, a))
