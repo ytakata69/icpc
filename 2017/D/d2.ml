341fcf35027adae556748891841a3a55f52838ba
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2022-01-30 16:18:42 +0900

    Unify solve_with_array into solve_with_map

--- 2017/D/d2.ml
+++ 2017/D/d2.ml
@@ -5,29 +5,7 @@
 
 open List
 
-(* 配列を使った動的計画法 (m < 23) *)
-let solve_with_array n m recipes =
-  (* intに変換 *)
-  let int_recipes = map (fun s -> int_of_string ("0b" ^ s)) recipes in
-  (* 2^m通りの各残り方に対し, それが得られるレシピ数 *)
-  let tbl = Array.make (1 lsl m) (-1) in
-  tbl.(0) <- 0;  (* 残り方0 -> 試したレシピ数0 *)
-
-  let final_tbl =
-    fold_left  (* レシピごとにtblを更新 *)
-      (fun tbl recipe ->
-         let newtbl = Array.copy tbl in
-         Array.iteri (fun i v ->
-           if v >= 0 then
-             let j = i lxor recipe in (* recipeを試したときの残り方 *)
-             newtbl.(j) <- max newtbl.(j) (v + 1))
-           tbl;
-         newtbl)
-      tbl int_recipes
-  in
-  (* 全材料の残りが0である最大レシピ数 *)
-  final_tbl.(0)
-
+(* 0-1文字列のXOR *)
 let xor s1 s2 =
   let m = String.length s1 in
   String.init m (fun i -> if s1.[i] <> s2.[i] then '1' else '0')
@@ -35,8 +13,8 @@ let xor s1 s2 =
 (* stringをキーとするMap *)
 module StrMap = Map.Make(String)
 
-(* Mapを使った動的計画法 (n <= 23) *)
-let solve_with_map n m recipes =
+(* Mapを使った動的計画法 *)
+let solve n m recipes =
   let zero = String.make m '0' in (* 全材料の残り0 *)
 
   (* 各残り方に対してそれが得られる最大レシピ数を返すMap *)
@@ -57,10 +35,6 @@ let solve_with_map n m recipes =
   in
   (* 全材料の残りが0となる最大レシピ数 *)
   StrMap.find zero final_tbl
-
-let solve n m recipes =
-  if n > m then solve_with_array n m recipes
-           else solve_with_map   n m recipes
 
 (* 標準入力から空白区切りの整数の列を読み出す *)
 let read_int_list () =
