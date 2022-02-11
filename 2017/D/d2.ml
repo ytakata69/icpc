eae97507ef9015270ed66bda25c2bd2be131e60c
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2022-02-11 13:31:22 +0900

    Refactoring the simpler code

--- 2017/D/d2.ml
+++ 2017/D/d2.ml
@@ -5,13 +5,13 @@
 
 open List
 
+(* stringをキーとするMap *)
+module StrMap = Map.Make(String)
+
 (* 0-1文字列のXOR *)
 let xor s1 s2 =
   let m = String.length s1 in
-  String.init m (fun i -> if s1.[i] <> s2.[i] then '1' else '0')
-
-(* stringをキーとするMap *)
-module StrMap = Map.Make(String)
+  String.init m (fun i -> if s1.[i] = s2.[i] then '0' else '1')
 
 (* Mapを使った動的計画法 *)
 let solve n m recipes =
@@ -20,21 +20,25 @@ let solve n m recipes =
   (* 各残り方に対してそれが得られる最大レシピ数を返すMap *)
   let init_tbl = StrMap.singleton zero 0 in
 
-  let final_tbl =
-    fold_left  (* レシピごとにtblを更新 *)
-      (fun cur_tbl recipe ->
-         StrMap.fold (fun k v tbl -> (* cur_tbl中の各残り方について *)
-             (* recipeを試したときの残り方と試したレシピ数 *)
-             let new_k = xor k recipe in
-             let new_v = if StrMap.mem new_k tbl
-                         then max (StrMap.find new_k tbl) (v + 1)
-                         else v + 1 in
-             StrMap.add new_k new_v tbl) (* 新しいtbl *)
-           cur_tbl cur_tbl)
-      init_tbl recipes
+  (* 1レシピに関してcur_tblを更新する (新しいMapを返す) 関数 *)
+  let update_table cur_tbl recipe =
+    (* cur_tbl中の各残り方kに対しrecipeを試したときの残り方を追加 *)
+    StrMap.fold
+      (fun k v tbl ->
+        let k' = xor k recipe in (* recipeを試した後の残り方 *)
+        if StrMap.mem k' tbl && StrMap.find k' tbl > v
+        then tbl (* 更新しない *)
+        else StrMap.add k' (v + 1) tbl) (* 更新したMapを返す *)
+      cur_tbl cur_tbl
   in
+
+  (* update_tableを全レシピに適用 *)
+  let final_tbl = fold_left update_table init_tbl recipes in
+
   (* 全材料の残りが0となる最大レシピ数 *)
   StrMap.find zero final_tbl
+
+(* ----- 以下は入出力 ----- *)
 
 (* 標準入力から空白区切りの整数の列を読み出す *)
 let read_int_list () =
