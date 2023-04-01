b0d12f5bff6e2e98c56cdd2729218afa80b4ac97
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2023-04-01 10:44:52 +0900

    Add a.hs

---
+++ 2022/a.hs
@@ -0,0 +1,19 @@
+#!/usr/bin/env runghc
+
+-- ACM-ICPC 2022 Domestic Qualifier
+-- Problem A: Counting Peaks of Infection
+
+import Control.Monad (when)
+
+main :: IO ()
+main = do
+    n <- read <$> getLine :: IO Int
+    when (n /= 0) $ do
+        vs <- map read . words <$> getLine :: IO [Int]
+        print $ numPeak vs
+        main
+
+numPeak :: [Int] -> Int
+numPeak vs =
+    length . filter (\(a,b,c) -> a < b && b > c)
+        $ zip3 vs (tail vs) (tail $ tail vs)
