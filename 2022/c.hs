e036369440c3f9b83dcb457fd1a10a5735ef6b38
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2023-04-02 00:14:14 +0900

    Add c.hs

---
+++ 2022/c.hs
@@ -0,0 +1,25 @@
+#!/usr/bin/env runghc
+
+-- ACM-ICPC 2022 Domestic Qualifier
+-- Problem C: Training Schedule for ICPC
+
+import Control.Monad (when)
+
+main :: IO ()
+main = do
+    [n, m] <- map read . words <$> getLine :: IO [Int]
+    when ((n, m) /= (0, 0)) $ do
+        print $ solve n m
+        main
+
+solve :: Int -> Int -> Int
+solve n 0 = n ^ 2
+solve n m = maximum $ map (splitRepose n m) [1..min m (n+1)]
+
+-- split reposes into s segments
+splitRepose :: Int -> Int -> Int -> Int
+splitRepose n m s = gain - lose
+    where s' = max (s - 1) 1  -- split trainings into s' segments
+          (m1, r) = m `divMod` s
+          lose = (m1 ^ 2) * (s - r) + ((m1 + 1) ^ 2) * r
+          gain = (n - (s' - 1)) ^ 2 + (s' - 1)
