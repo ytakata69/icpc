dd9432063976fb7acff346eb4900b4ca45bde138
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2023-04-18 18:15:02 +0900

    Speed up by avoiding using sets of tuples

--- 2021/d.py
+++ 2021/d.py
@@ -5,20 +5,39 @@
 def normpair(a, b):
     return (a, b) if a <= b else (b, a)
 
+def add(dic, t):
+    k, v = t
+    if k not in dic:
+        dic[k] = set()
+    dic[k].add(v)
+
+def update(dic1, dic2):
+    for k in dic2:
+        if k not in dic1:
+            dic1[k] = set()
+        dic1[k].update(dic2[k])
+
+def each(dic):
+    for k in dic:
+        for v in dic[k]:
+            yield (k, v)
+
 def solve(bs):
     total = sum(bs)
     onethird = total // 3
-    dp = {(0, 0)}  # the numbers of baloons processed by two workers
+    dp = {}
+    add(dp, (0, 0)) # the numbers of baloons processed by two workers
     for b in bs:
-        nextdp = set()
-        for b1, b2 in dp:
-            if b1 < onethird:
-                nextdp.add(normpair(b1 + b, b2))
-            if b2 < onethird:
-                nextdp.add(normpair(b1, b2 + b))
-        dp.update(nextdp)
+        nextdp = {}
+        for b1 in dp:
+            for b2 in dp[b1]:
+                if b1 < onethird:
+                    add(nextdp, normpair(b1 + b, b2))
+                if b2 < onethird:
+                    add(nextdp, normpair(b1, b2 + b))
+        update(dp, nextdp)
 
-    return max(min(b1, b2, total - b1 - b2) for b1, b2 in dp)
+    return max(min(b1, b2, total - b1 - b2) for b1, b2 in each(dp))
 
 while True:
     n = int(input())
