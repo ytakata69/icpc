c793ca3130466e47929c720d7cf356262638bc1a
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2023-07-08 16:29:19 +0900

    Simplify using sets

--- 2023/d.py
+++ 2023/d.py
@@ -2,58 +2,25 @@
 
 # Problem D - Efficient Problem Set
 
-def included(a, b):
-    """a, b are bit-vectors."""
-    return (a & ~b) == 0
-
-def notcovered(a, b):
-    nc = a & ~b
-    i = 0
-    while nc != 0:
-        if nc & 1:
-            return i
-        nc >>= 1
-        i += 1
-    raise
-
-def values(a):
-    i = 0
-    while a != 0:
-        if a & 1:
-            yield i
-        a >>= 1
-        i += 1
-
-def add(a, v):
-    r = a
-    for w in values(a):
-        r |= 1 << (w + v)
-    return r
-
 def solve(n, s):
-    # 集合をビットベクトルで表す
-    s = s.translate(str.maketrans('xo', '01'))  # xo -> 01
-    s = int(''.join(reversed(s)), 2)  # to binary int
+    s = set(i for i, c in enumerate(s) if c == 'o') # setに変換
 
-    frontier = {1}  # ビットベクトルの集合 (1 means {0})
-    over = 1 << (n + 1)  # nより大きい数を含む集合はだめ
-
+    frontier = [{0}]
     count = 0  # 問題数
     while True:
-        newfront = set()
+        newfront = []
         for f in frontier:
-            if included(s, f):   # 必要な得点がすべてカバーされた
+            if s <= f:        # 必要な得点がすべてカバーされた
                 return count
-            v = notcovered(s, f) # まだカバーされていない最小の得点
-            for w in values(f):  # カバー済みの各値
-                if w >= v:
-                    break
-                nf = add(f, v - w)  # (v - w)点の問題を加えた集合
-                if nf < over and nf not in frontier:
-                    newfront.add(nf)
+            v = min(s - f)    # まだカバーされていない最小の得点
+            rest = n - max(f) # 残り得点
+            for w in f:       # カバー済みの各値
+                if 0 < v - w <= rest:
+                    # fに(v - w)点の問題を加えた集合
+                    nf = f | set(e + v - w for e in f)
+                    newfront.append(nf)
         frontier = newfront
         count += 1
-        #print(','.join(map(lambda v: format(v, 'b'), frontier)))
 
 
 while True:
