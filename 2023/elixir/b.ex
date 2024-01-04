a69f5508f618bc82c543ecc05b57ce64b10e2557
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2024-01-04 12:03:04 +0900

    Use Enum.scan

--- 2023/elixir/b.ex
+++ 2023/elixir/b.ex
@@ -19,17 +19,17 @@ defmodule Main do
   end
 
   def solve(_n, _m, p, q, x) do
-    # Pから下に進んだときの位置の軌跡 (の逆順)
+    # Pから下に進んだときの位置の軌跡
     tp = trace(p, x)
 
-    if hd(tp) == q do
+    if List.last(tp) == q do
       "OK"  # Qに到達
     else
       # Qから上に進んだときの位置の軌跡
       tq = trace(q, Enum.reverse(x))
 
       # Pからの軌跡とQからの軌跡が距離1に初めてなる位置を探す
-      case Enum.zip(Enum.reverse(tp), tq)
+      case Enum.zip(tp, Enum.reverse(tq))
            |> Enum.split_while(fn {a, b} -> abs(a - b) > 1 end) do
         {_, []} -> "NG"
         {l, [{h1, h2}|_]} -> {min(h1, h2), length(l)}
@@ -37,15 +37,15 @@ defmodule Main do
     end
   end
 
-  # 横線のリストxによるpの軌跡 (の逆順)
+  # 横線のリストxによるpの軌跡
   def trace(p, x) do
-    Enum.reduce(x, [p], &([cross(hd(&2), &1) | &2]))
+    [p] ++ Enum.scan(x, p, &cross/2)
   end
 
   # 横線xによるpの移動
-  def cross(p, x) when p == x,     do: p + 1
-  def cross(p, x) when p == x + 1, do: p - 1
-  def cross(p, _x), do: p
+  def cross(x, p) when p == x,     do: p + 1
+  def cross(x, p) when p == x + 1, do: p - 1
+  def cross(_, p), do: p
 end
 
 # Remove the following line when you run this code on AtCoder
