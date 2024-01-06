a7ed4dbbda76c3edbf426dea19c5e701bbbfa247
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2024-01-06 11:40:44 +0900

    Simpler algorithm

--- 2022/b2.hs
+++ 2022/b2.hs
@@ -6,46 +6,30 @@
 import Control.Monad (when, replicateM)
 import Control.Applicative ((<$>))
 import Data.List (sort)
-import qualified Data.Map as Map
 
 main :: IO ()
 main = do
     n <- read <$> getLine :: IO Int
     when (n /= 0) $ do
         hs <- replicateM n (map read . words <$> getLine) :: IO [[Int]]
-        let hs' = Map.fromList $ zip [0..] hs
-        print $ solve n (0, hs')
+        print $ solve n hs
         main
 
 type Hand = [Int]
-type HandMap = Map.Map Int Hand
-type Position = (Int, HandMap)
+type HandQueue = ([Hand], [Hand])  -- (queue, back_queue)
 
-solve :: Int -> Position -> Int
-solve n (p, hs) =
-    length . takeWhile ((> 0) . Map.size . snd) $ iterate (play n) (p, iniths)
-    where iniths = Map.filter (not . null) $ Map.map discardDup hs
+solve :: Int -> [Hand] -> Int
+solve n hs =
+    length . takeWhile (not . nullQueue) $ iterate play (hs', [])
+    where dupHand [c1, c2] = c1 == c2
+          nullQueue (q1, q2) = null q1 && null q2
+          hs' = filter (not .dupHand) hs
 
-play :: Int -> Position -> Position
-play n (p, hs)
-    | Map.size hs == 0 = (p, hs)
-    | not (Map.member p hs) = play n (nextPlayer n p hs, hs)
-    | otherwise = (nextPlayer n p newhs, newhs)
-      where q = nextPlayer n p hs
-            Just h1 = Map.lookup p hs
-            Just h2 = Map.lookup q hs
-            (c1 : h1') = sort h1
+play :: HandQueue -> HandQueue
+play ([],  rest) = play (reverse rest, [])
+play ([h], rest) = play (h : reverse rest, [])
+play (h1 : h2 : rest1, rest2) = (q1, q2)
+      where c1 : h1' = sort h1
             h2' = if c1 `elem` h2 then filter (/= c1) h2 else c1 : h2
-            newhs = insertOrDelete q h2' . insertOrDelete p h1' $ hs
-
-nextPlayer :: Int -> Int -> HandMap -> Int
-nextPlayer n p hs = if Map.member q hs then q else nextPlayer n q hs
-    where q = (p + 1) `mod` n
-
-discardDup :: Hand -> Hand
-discardDup h@[c1, c2]
-    | c1 == c2  = []
-    | otherwise = h
-
-insertOrDelete :: Ord k => k -> [a] -> Map.Map k [a] -> Map.Map k [a]
-insertOrDelete k v = if null v then Map.delete k else Map.insert k v
+            q1  = if null h2' then rest1 else h2' : rest1
+            q2  = if null h1' then rest2 else h1' : rest2
