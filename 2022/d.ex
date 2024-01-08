e8186acbf7f50c950f70af38e0a4e7600b11c10f
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2024-01-08 17:49:07 +0900

    Refactoring a little

--- 2022/d.ex
+++ 2022/d.ex
@@ -17,33 +17,31 @@ defmodule Main do
     end
   end
 
-  # k個のゲートを使ってssを入場順tsに並べ替える方法の個数
+  # k個のゲートを使って入場待ち列ssを入場列tsに並べ替える方法の個数を計算.
+  # ss: 入場待ち順の座席番号のリスト, ts: 入場順の座席番号のリスト
   def solve(n, k, ss, ts) do
-    # 座席番号の列ssを入場順の列に変換
-    ent = Enum.zip(ts, 0..n-1) |> Map.new
-    es = ss |> Enum.map(&(Map.get(ent, &1)))
+    # 座席番号 → 入場順 であるMap
+    entord = Enum.zip(ts, 0..n-1) |> Map.new
 
-    # 必ず切断すべき位置 (前の人より先に入場) を表すbooleanリスト
-    mustcut = Enum.zip_with([hd(es) | es], es, &(&1 > &2))
+    # 入場待ち列上 必ず切断すべき位置 (前の人より先に入場) を表すboolean列
+    es = Enum.map(ss, &(Map.get(entord, &1)))  # 各人の入場順
+    mustcut = Enum.zip_with(es, tl(es), &(&1 > &2))
 
-    # mustcutで分割したときの各座席番号のグループ番号
-    gs = Enum.scan(mustcut, 0, &(if &1, do: &2 + 1, else: &2))
-    group = Enum.zip(ss, gs) |> Map.new
-
-    # 切断できない位置 (大きい座席番号より後に入場) を表すbooleanリスト
+    # 入場列上 切断できない位置 (大きい座席番号より後に入場) を表すboolean列
     premax = Enum.scan(ts, &max/2)  # その位置までの最大座席番号
-    notcut = premax |> Enum.zip_with(ts, &(&1 > &2))
+    notcut = Enum.zip_with(premax, ts, &(&1 > &2))
 
+    # 切断可能位置のうち残りゲート数個を選ぶ選び方が答
     nmustcut = mustcut |> Enum.count(&(&1))  # trueの個数
     nnotcut  = notcut  |> Enum.count(&(&1))
-    m = n - 1 - nmustcut - nnotcut  # 切断候補数
+    m = n - 1 - nmustcut - nnotcut  # 切断可能位置の数
     l = k - 1 - nmustcut            # 残りゲート数
 
-    # 大きい座席番号が別のグループにいる場合, 実現できない
-    conflict = Enum.zip_with(premax, ts,
-                             &(Map.get(group, &1) != Map.get(group, &2)))
-               |> Enum.zip_with(notcut, &(&1 && &2))
-               |> Enum.reduce(&(&1 || &2))
+    # mustcutで分割したとき大きい座席番号が別のゲートにいるなら答は0
+    gs = [0] ++ Enum.scan(mustcut, 0, &(if &1, do: &2 + 1, else: &2))
+    gate = Enum.zip(ss, gs) |> Map.new  # 座席番号 → ゲート番号
+    conflict = Enum.zip_with(premax, ts, &(&1 > &2 && gate[&1] != gate[&2]))
+               |> Enum.any?
 
     if l < 0 || conflict, do: 0, else: Modulo.sum_comb(m, l)
   end
@@ -56,19 +54,17 @@ defmodule Modulo do
   def sum_comb(_, 0), do: 1
   def sum_comb(m, l) do
     Enum.scan(1..min(m, l), 1, &(mdiv(&2 * (m - &1 + 1), &1)))
-      |> Enum.reduce(&mplus/2)
-      |> mplus(1)  # comb(m, 0)を加算
+      |> Enum.reduce(&madd/2)
+      |> madd(1)  # comb(m, 0) (= 1) を加算
   end
 
-  def mplus(a, b), do: rem(a + b, @modulus)
-
   def mpow(_, 0), do: 1
-  def mpow(x, 1), do: rem(x, @modulus)
   def mpow(x, p) do
     k = if rem(p, 2) == 0, do: 1, else: x
     rem(k * mpow(rem(x * x, @modulus), div(p, 2)), @modulus)
   end
 
+  def madd(a, b), do: rem(a + b, @modulus)
   def mdiv(n, d), do: rem(n * mpow(d, @modulus - 2), @modulus)
 end
 
