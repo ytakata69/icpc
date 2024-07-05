b661202563a4d6598227d81a0797d9d35479a67e
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2024-07-05 20:27:44 +0900

    Simplify 2024/b.py

--- 2024/b.py
+++ 2024/b.py
@@ -4,24 +4,24 @@
 # Problem B - Overtaking
 
 def accum(a):
+    """累積和のリスト"""
     ac = [a[0]]
     for i in range(1, len(a)):
         ac.append(ac[-1] + a[i])
     return ac
 
-def sign(ab):
-    ai, bi = ab
+def sign(ai, bi):
     return -1 if ai < bi else 1
 
 def solve(a, b):
-    acc_a = accum(a)
-    acc_b = accum(b)
-    pairs = [(ai, bi) for ai, bi in zip(acc_a, acc_b) if ai != bi]
-    ovtks = []
-    for ai, bi in pairs:
-        if ovtks == [] or sign(ovtks[-1]) != sign((ai, bi)):
-            ovtks.append((ai, bi))
-    return len(ovtks) - 1
+    acc_a = accum(a)  # 累積和のリスト
+    acc_b = accum(b)  # 〃
+
+    # 差の符号のリスト（0を除去）
+    signs = [sign(ai, bi) for ai, bi in zip(acc_a, acc_b) if ai != bi]
+
+    # 符号が変化する位置の個数
+    return sum(1 for i in range(1, len(signs)) if signs[i-1] != signs[i])
 
 while True:
     n = int(input())
