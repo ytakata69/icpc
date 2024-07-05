40f1c4b0ed057e42b3db67fc41a5ca6435b7c33d
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2024-07-05 20:52:26 +0900

    Reduce code clone

--- 2024/d.py
+++ 2024/d.py
@@ -6,27 +6,43 @@
 import bisect
 
 def approach(f, t, d):
-    """fからtへ距離d以下だけ移動したときの移動先と残り距離"""
+    """（XまたはY座標のみ）fからtへ距離d以下だけ移動した移動先と残り距離"""
     rd = min(d, abs(f - t))
     return f + rd * (1 if t > f else -1), d - rd
 
-def solve(a, b, d, xy):
-    obstx = {}  # 同じX座標の障害物のリスト
-    obsty = {}  # 同じY座標の障害物のリスト
-    for x, y in xy:
-        if x not in obstx:
-            obstx[x] = []
-        if y not in obsty:
-            obsty[y] = []
-        obstx[x].append(y)
-        obsty[y].append(x)
-    for x in obstx:
-        obstx[x].sort()  # 昇順に整列
-    for y in obsty:
-        obsty[y].sort()  # 昇順に整列
+def create_obst_list(xy, idx):
+    """同じXまたはY座標の障害物のリストの辞書"""
+    obst = {}
+    for xyi in xy:
+        x, y = xyi[idx], xyi[1 - idx]
+        if x not in obst:
+            obst[x] = []
+        obst[x].append(y)
+    for x in obst:
+        obst[x].sort()  # 昇順に整列
+    return obst
 
-    visited = {}
+def move(obst, x, y, d, vec):
+    """(x,y)からX方向に向きvecに距離dだけ移動"""
+    # 同じY座標に障害物なし
+    if y not in obst:
+        return x + d * vec, 0
+
+    # 二分探索: obst[y][i-1] <= x < obst[y][i] となるiを返す
+    i = bisect.bisect(obst[y], x)
+    if (vec > 0 and i >= len(obst[y])) or (vec < 0 and i <= 0):
+        # 進行方向に障害物なし
+        return x + d * vec, 0
+    else:
+        tx = obst[y][i] - 1 if vec > 0 else obst[y][i - 1] + 1
+        return approach(x, tx, d)
+
+def solve(a, b, d, xy):
+    obstx = create_obst_list(xy, 0)  # 同じX座標の障害物のリスト
+    obsty = create_obst_list(xy, 1)  # 同じY座標〃
+
     dir = 0  # 0=east, 1=north, 2=west, 3=south
+    visited = {}
     while d > 0:
         # 同じ座標・方向に再度到達
         if (a, b, dir) in visited:
@@ -37,35 +53,14 @@ def solve(a, b, d, xy):
         else:
             visited[(a, b, dir)] = d
 
-        # 障害物なし
-        if dir % 2 == 0 and b not in obsty:
-            a += d * (1 if dir == 0 else -1)
-            d = 0
-        elif dir % 2 == 1 and a not in obstx:
-            b += d * (1 if dir == 1 else -1)
-            d = 0
+        # 進行方向
+        vec = 1 if dir < 2 else -1
 
-        # 障害物あり
-        elif dir % 2 == 0:  # 東西
-            i = bisect.bisect(obsty[b], a)
-            # 進行方向に障害物なし
-            if (dir == 0 and i >= len(obsty[b])) or \
-               (dir == 2 and i <= 0):
-                a += d * (1 if dir == 0 else -1)
-                d = 0
-            else:
-                ta = obsty[b][i] - 1 if dir == 0 else obsty[b][i - 1] + 1
-                a, d = approach(a, ta, d)
+        if dir % 2 == 0:  # 東西
+            a, d = move(obsty, a, b, d, vec)
         else:  # 南北
-            i = bisect.bisect(obstx[a], b)
-            # 進行方向に障害物なし
-            if (dir == 1 and i >= len(obstx[a])) or \
-               (dir == 3 and i <= 0):
-                b += d * (1 if dir == 1 else -1)
-                d = 0
-            else:
-                tb = obstx[a][i] - 1 if dir == 1 else obstx[a][i - 1] + 1
-                b, d = approach(b, tb, d)
+            b, d = move(obstx, b, a, d, vec)
+
         dir = (dir + 1) % 4
     return (a, b)
 
