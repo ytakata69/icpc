e6b606928f8dc247b6a99736b28c287ba49ac8bb
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2025-12-10 17:43:56 +0900

    Update 2024/[ab].hs (for readability)

--- 2024/a.hs
+++ 2024/a.hs
@@ -3,16 +3,19 @@
 -- ICPC Domestic Qualifier
 -- Problem A - Snacks within 300 Yen
 
-import Control.Monad (when)
-import Control.Applicative ((<$>))
-
+-- 入力を受け取って結果をprintするだけのmain関数
 main :: IO ()
-main = do
-    n <- read <$> getLine :: IO Int
-    when (n /= 0) $ do
-        as <- map read . words <$> getLine :: IO [Int]
-        print $ shopping as
-        main
+main = do n <- read <$> getLine
+          if n == 0
+             then return ()
+             else do prices <- map read . words <$> getLine
+                     print $ solve prices
+                     main
 
-shopping :: [Int] -> Int
-shopping = foldl (\s a -> if s + a <= 300 then s + a else s) 0
+-- アルゴリズム本体
+solve :: [Int] -> Int
+solve = foldl buyGreedy 0
+  where
+    buyGreedy bought price
+      | bought + price <= 300 = bought + price
+      | otherwise             = bought
--- 2024/b.hs
+++ 2024/b.hs
@@ -3,22 +3,23 @@
 -- ICPC Domestic Qualifier
 -- Problem B - Overtaking
 
-import Control.Monad (when)
-import Control.Applicative ((<$>))
-
+-- 入力を受け取って結果をprintするだけのmain関数
 main :: IO ()
-main = do
-    n <- read <$> getLine :: IO Int
-    when (n /= 0) $ do
-        as <- map read . words <$> getLine :: IO [Int]
-        bs <- map read . words <$> getLine :: IO [Int]
-        print $ overtaking as bs
-        main
+main = do n <- read <$> getLine
+          if n == 0
+             then return ()
+             else do as <- map read . words <$> getLine
+                     bs <- map read . words <$> getLine
+                     print $ solve as bs
+                     main
 
-overtaking :: [Int] -> [Int] -> Int
-overtaking as bs = length ovrs
-    where
-        acc_as = scanl (+) 0 as  -- 累積和
-        acc_bs = scanl (+) 0 bs
-        sgns = [compare a b | (a,b) <- zip acc_as acc_bs, a /= b]
-        ovrs = [1 | (a,b) <- zip sgns (tail sgns), a /= b] -- 符号変化位置
+-- アルゴリズム本体
+solve :: [Int] -> [Int] -> Int
+solve as bs =
+    -- 符号が変化する位置の個数
+    length [1 | (s1, s2) <- zip signs (tail signs), s1 /= s2]
+  where
+    acc_a = scanl (+) 0 as  -- 累積和のリスト
+    acc_b = scanl (+) 0 bs  -- 〃
+    -- 差の符号のリスト（ただし0を除去）
+    signs = [signum (a - b) | (a, b) <- zip acc_a acc_b, a /= b]
